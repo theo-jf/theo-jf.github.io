f7e64ddef0b54a8ef0a85855c8c1a5952c2fab4c
Theo Janke-Furman <tjankefurman@gmail.com>
2022-10-10 20:29:15 -0500

    added project titles

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -12,40 +12,44 @@ export default function Projects() {
         <h2 className="projectsHeader bg-dark">&#x2193; Projects &#x2193;</h2>
         <h2 className="aboutHeader bg-white">&#x2191; About &#x2191;</h2>
         <Box height="10vh" className="divider" />
-        <Grid style={{overflow: "auto"}} justifyContent="space-evenly" height="75vh" container spacing={1}>
+        <Grid style={{overflow: "auto"}} justifyContent="space-evenly" height="75vh" container rowSpacing={0} columnSpacing={10}>
             <Grid item>
                 <CardContent>
-                    <h2>Thing</h2>
+                    <h2>Book Snoop</h2>
+                    <p>TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT Text TExt</p>
+                </CardContent>
+            </Grid>
+            <Grid item>
+                <CardContent>
+                    <h2>Prime Group Project</h2>
+                    <p>More details coming soon!</p>
+                </CardContent>
+            </Grid>
+            <Grid item>
+                <CardContent>
+                    <h2>Cat Gallery</h2>
+                    <p>Look at pictures of my cats!</p>
+                    <p id="catGallery">Visit on GitHub</p>
+                    <p>Tech used:</p>
+                </CardContent>
+            </Grid>
+            <Grid item>
+                <CardContent>
+                    <h2>Feedback Form</h2>
+                    <p>Stuff</p>
+                    <p id="feedbackForm">Visit on GitHub</p>
+                    <p>Tech used:</p>
+                </CardContent>
+            </Grid>
+            <Grid item>
+                <CardContent>
+                    <h2>JQuery Fullstack Todo List</h2>
                     <p>TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT</p>
                 </CardContent>
             </Grid>
             <Grid item>
                 <CardContent>
-                    <h2>Thing</h2>
-                    <p>TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT</p>
-                </CardContent>
-            </Grid>
-            <Grid item>
-                <CardContent>
-                    <h2>Thing</h2>
-                    <p>TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT</p>
-                </CardContent>
-            </Grid>
-            <Grid item>
-                <CardContent>
-                    <h2>Thing</h2>
-                    <p>TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT</p>
-                </CardContent>
-            </Grid>
-            <Grid item>
-                <CardContent>
-                    <h2>Thing</h2>
-                    <p>TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT</p>
-                </CardContent>
-            </Grid>
-            <Grid item>
-                <CardContent>
-                    <h2>Thing</h2>
+                    <h2>JQuery Server Calculator</h2>
                     <p>TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT</p>
                 </CardContent>
             </Grid>
